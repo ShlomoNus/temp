25e82b5416a0e725bfadcda8e05e4f1122f56cb4
Shlomo Nusbaum <shlomonuswork@gmail.com>
2023-10-29 09:52:58 +0200

    delete web server

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const fs = require('fs');
 const nodemailer = require('nodemailer');
 const handlebars = require('handlebars');
-const {  p1,p2,p3,listItems} = require('./consts');
+const {  footer_text_1,footer_text_2,header_text,listIntro_1_text,listIntro_2_text,listItems,main_text,orders_text_1,orders_text_2,orders_text_3} = require('./consts');
 
 // Read the HTML email template
-const emailTemplate = fs.readFileSync('email-template.hbs', 'utf8');
-
+const templateSource = fs.readFileSync(
+    __dirname + "/views/email-template.hbs",
+    "utf8"
+  );
 // Compile the Handlebars template
-const compiledTemplate = handlebars.compile(emailTemplate);
+const compiledTemplate = handlebars.compile(templateSource);
 
 // Data to fill in the template
 const emailData = {
-    title: 'My Email',
-    heading: 'Welcome to my email',
-    p1,
-    name:'שלמה',
-    role:'מפתח',
-    department:'משרד התיכנון',
-    p2,
-    p3,
-    listItems
-};
+    title: "My Email",
+    heading: "Welcome to my email",
+    header_text,
+    name: "שלמה",
+    role: "מפתח",
+    department: "משרד התיכנון",
+    listIntro_1_text,
+    listIntro_2_text,
+    main_text,
+    orders_text_1,
+    orders_text_2,
+    orders_text_3,
+    footer_text_1,
+    footer_text_2,
+    listItems,
+  };
 
 // Use Nodemailer to send the email
 async function sendEmail() {
@@ -43,6 +51,8 @@ async function sendEmail() {
         html: compiledTemplate(emailData),
     };
 
+
+    
     try {
         const info = await transporter.sendMail(mailOptions);
         console.log('Email sent:', info.response);
