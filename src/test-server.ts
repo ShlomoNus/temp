d3c37c876e8c41efea396c2dfac5e4d372043d4f
ShlomoNus <shlomonuswork@gmail.com>
2026-05-10 12:34:41 +0300

    refactor: replace console logging with logger utility for improved logging consistency

--- src/test-server.ts
+++ src/test-server.ts
@@ -1,7 +1,8 @@
 import { app } from "./app";
+import { logger } from "./utils/logger";
 
 const PORT = Number(process.env.PORT) || 3080;
 
 app.listen(PORT, () => {
-  console.info(`Test server at http://127.0.0.1:${PORT}`);
+  logger.info(`Test server at http://127.0.0.1:${PORT}`);
 });
