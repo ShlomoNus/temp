22a4d59366d19abdfaf8882dbc1ed3032950c42e
ShlomoNus <shlomonuswork@gmail.com>
2026-05-20 12:35:28 +0000

    style: format RequireAtLeastOne type definition for improved readability

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,5 +1,5 @@
-export type RequireAtLeastOne<T, Keys extends keyof T> =
-  Omit<T, Keys> &
-  {
-    [K in Keys]: Required<Pick<T, K>> & Partial<Omit<Pick<T, Keys>, K>>
-  }[Keys];
+export type RequireAtLeastOne<T, Keys extends keyof T>
+  = Omit<T, Keys>
+    & {
+      [K in Keys]: Required<Pick<T, K>> & Partial<Omit<Pick<T, Keys>, K>>
+    }[Keys];
